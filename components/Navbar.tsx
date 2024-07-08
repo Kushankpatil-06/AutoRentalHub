f9393d7f09437b9bfaa416a71c04b1472fecc032
KUSHANK PATIL <patilkushank@gmail.com>
2024-07-08 10:44:27 +0530

    imports

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 import CustomButton from "./CustomButton";
 
-const NavBar = () => (
+const Navbar = () => (
   <header className='w-full  absolute z-10'>
     <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
       <Link href='/' className='flex justify-center items-center'>
@@ -25,4 +25,4 @@ const NavBar = () => (
   </header>
 );
 
-export default NavBar;
+export default Navbar;
